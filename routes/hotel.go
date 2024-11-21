e9562d34e7b5291b669cf82d74783eb4cc957026
Jeerasak Kajonrattanavanit <mazuroze@gmail.com>
2024-11-21 18:30:23 +0700

    add middleware to hotel routes

--- routes/hotel.go
+++ routes/hotel.go
@@ -2,17 +2,18 @@ package routes
 
 import (
 	"github.com/JongSinister/WTFiber/controllers"
+	"github.com/JongSinister/WTFiber/middleware"
 	"github.com/gofiber/fiber/v2"
 )
 
 func HotelRoutes(router fiber.Router) {
 	router.Get("/", controllers.GetHotels)
 	router.Get("/:id", controllers.GetHotel)
-	router.Post("/", controllers.CreateHotel)
-	router.Put("/:id", controllers.UpdateHotel)
+	router.Post("/", middleware.Protect, middleware.Authorize("admin"), controllers.CreateHotel)
+	router.Put("/:id", middleware.Protect, middleware.Authorize("admin"), controllers.UpdateHotel)
 	router.Delete("/:id", controllers.DeleteHotel)
 
 	// Create a appointment for a hotel
-	router.Post("/:hotelId/appointments", controllers.AddAppointment)
+	router.Post("/:hotelId/appointments", middleware.Protect, controllers.AddAppointment)
 
 }
